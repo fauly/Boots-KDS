3b81f8664547c199f7ee5fc237d456ecd91d7e34
fauly <faulyfaux@gmail.com>
2024-05-07 18:36:20 +0100

    setup correct routing

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ function verifySignature(body, signature, key, url) {
     return hash === signature;
 }
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
