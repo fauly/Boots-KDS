8617e74bf581ec9c0a58576204f8ebe8945a92d9
fauly <faulyfaux@gmail.com>
2024-05-07 17:42:00 +0100

    db altered to fix warnings

--- db.js
+++ db.js
@@ -1,17 +1,13 @@
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => {
+      console.error('MongoDB connection error:', err);
+      console.log('Failed to connect to MongoDB at:', process.env.MONGODB_URI);
     });
-    console.log('MongoDB connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);  // Exit process with failure
-  }
 }
 
 module.exports = connectDB;
