832a12a56a0f851e95fea9a94725407feb310997
fauly <faulyfaux@gmail.com>
2024-05-07 18:32:04 +0100

    ag

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ function verifySignature(body, signature, key, url) {
     return hash === signature;
 }
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on HTTPS port ${PORT}`);
