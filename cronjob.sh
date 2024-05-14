d26517faaf9a6e28ef9c78ac694548668697b4ea
fauly <faulyfaux@gmail.com>
2024-05-14 15:48:09 +0100

    added pm2 save, defined path variables, created log function

--- cronjob.sh
+++ cronjob.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
 # Add the node bin directory to the PATH
-PATH=$PATH:~/.nvm/versions/node/v21.5.0/bin
+PATH=$PATH:$HOME/.nvm/versions/node/v21.5.0/bin
+
+# Define the PM2 binary path
+PM2=$HOME/.nvm/versions/node/v21.5.0/bin/pm2
+
+# Log file path
+LOG_FILE=$HOME/public_html/kds.dirtyboots.cafe/cronjob.log
+
+# Function to log messages
+log_message() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> $LOG_FILE
+}
 
 # Check if pm2 is running
-~/.nvm/versions/node/v21.5.0/bin/pm2 ps | grep 'KDS'
+$PM2 ps | grep 'KDS'
 if [ $? -eq 0 ]; then
-  echo "Process running."
+  log_message "Process running."
+  # Optional: Save PM2 process list to synchronize
+  $PM2 save
 else
-  echo "Process not running."
+  log_message "Process not running. Starting process."
   cd /home/h80bi8l0u1q9/public_html/kds.dirtyboots.cafe
-  ~/.nvm/versions/node/v21.5.0/bin/pm2 start pm2.config.js --env production
+  $PM2 start pm2.config.js --env production
+  if [ $? -eq 0 ]; then
+    log_message "Process started successfully."
+    # Save PM2 process list after starting the process
+    $PM2 save
+  else
+    log_message "Failed to start the process."
+  fi
 fi
