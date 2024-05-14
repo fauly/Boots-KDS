d9e89854e5902e0d238424a80568d6fb9ac00417
fauly <faulyfaux@gmail.com>
2024-05-14 15:43:12 +0100

    cronjob change path

--- cronjob.sh
+++ cronjob.sh
@@ -4,11 +4,11 @@
 PATH=$PATH:/home/user/.nvm/versions/node/v21.5.0/bin
 
 # Check if pm2 is running
-/home/user/.nvm/versions/node/v21.5.0/bin/pm2 ps | grep 'KDS'
+~/.nvm/versions/node/v21.5.0/bin/pm2 ps | grep 'KDS'
 if [ $? -eq 0 ]; then
   echo "Process running."
 else
   echo "Process not running."
   cd /home/h80bi8l0u1q9/public_html/kds.dirtyboots.cafe
-  /home/user/.nvm/versions/node/v21.5.0/bin/pm2 start pm2.config.js --env production
+  ~/.nvm/versions/node/v21.5.0/bin/pm2 start pm2.config.js --env production
 fi
