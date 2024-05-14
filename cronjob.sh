38ddc175125249f46e0d626d5ff5e343c1f259b5
fauly <faulyfaux@gmail.com>
2024-05-14 15:41:11 +0100

    explicit pm2 path for cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# Add the node bin directory to the PATH
 PATH=$PATH:/home/user/.nvm/versions/node/v21.5.0/bin
-pm2 ps | grep 'KDS'
+
+# Check if pm2 is running
+/home/user/.nvm/versions/node/v21.5.0/bin/pm2 ps | grep 'KDS'
 if [ $? -eq 0 ]; then
   echo "Process running."
 else
   echo "Process not running."
   cd /home/h80bi8l0u1q9/public_html/kds.dirtyboots.cafe
-  pm2 start pm2.config.js --env production
+  /home/user/.nvm/versions/node/v21.5.0/bin/pm2 start pm2.config.js --env production
 fi
